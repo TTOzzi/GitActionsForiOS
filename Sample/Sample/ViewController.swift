eb5eb10ffa5b216d31d1904079a5f357f175ef73
TTOzzi <devttozzi@gmail.com>
2020-10-31 18:11:03 +0900

    애플 아이디로 로그인 구현

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -6,11 +6,56 @@
 //
 
 import UIKit
+import AuthenticationServices
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureAppleLoginButton()
     }
 }
 
+private extension ViewController {
+    func configureAppleLoginButton() {
+        let authorizationButton = ASAuthorizationAppleIDButton()
+        authorizationButton.translatesAutoresizingMaskIntoConstraints = false
+        authorizationButton.addTarget(self,
+                                      action: #selector(appleLoginButtonTapped),
+                                      for: .touchUpInside)
+        view.addSubview(authorizationButton)
+        NSLayoutConstraint.activate([
+            authorizationButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            authorizationButton.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    @objc func appleLoginButtonTapped() {
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+}
+
+extension ViewController: ASAuthorizationControllerDelegate {
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        guard let credential = authorization.credential as? ASAuthorizationAppleIDCredential else { return }
+        print(credential.user)
+        print(credential.fullName)
+        print(credential.email)
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print(error)
+    }
+}
+
+extension ViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window ?? ASPresentationAnchor()
+    }
+}
